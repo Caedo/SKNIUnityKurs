316861cc585848e78bed59a6974a198f494459d2
Pawel <dziedzicpm@gmail.com>
2018-03-30 19:06:36 +0200

    Starting Project. Added Cinamachine, Tank Controller (not completed) and created project structure

---
+++ 1.Wstęp/Assets/Completed/Scripts/TankController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Completed {
+	public class TankController : MonoBehaviour {
+		
+		public float m_Speed;
+
+		private Transform m_Camera;
+		private Rigidbody m_Rigidbody;
+
+		private void Awake() {
+			m_Camera = Camera.main.transform;
+			m_Rigidbody = GetComponent<Rigidbody>();
+		}
+
+		void FixedUpdate() {
+			var h = Input.GetAxisRaw("Horizontal");
+			var v = Input.GetAxisRaw("Vertical");
+
+			var m_CamForward = Vector3.Scale(m_Camera.forward, new Vector3(1, 0, 1)).normalized;
+			Vector3 moveDelta = m_CamForward * v + m_Camera.right * h;
+			moveDelta *= m_Speed;
+
+			m_Rigidbody.velocity = moveDelta;
+		}
+	}
+}
