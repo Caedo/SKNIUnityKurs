d96339a82b45bfdfc9a4a3da4c1b0cacc2e35d58
Pawel <dziedzicpm@gmail.com>
2018-03-31 13:17:21 +0200

    Commented some code

--- 1.Wstęp/Assets/Completed/Scripts/Bullet.cs
+++ 1.Wstęp/Assets/Completed/Scripts/Bullet.cs
@@ -7,18 +7,22 @@ namespace Completed {
 	[RequireComponent(typeof(Rigidbody))]
 	public class Bullet : MonoBehaviour {
 
-		public float m_Speed;
+		public float m_Speed; //Startowa prędkość pocisku
 
-		private Rigidbody m_Rigidbody;
+		private Rigidbody m_Rigidbody; //Referencja do komponentu Rigidbody 
 		private void Awake() {
+			//Pobierz komponent
 			m_Rigidbody = GetComponent<Rigidbody>();
 		}
 
 		private void Start() {
+			//ustal prędkość pocisku
 			m_Rigidbody.velocity = transform.forward * m_Speed;
 		}
 
-		public void Initialize(){
+		//Funkcja wywoływana z CannonController. Zrobiona tylko po to, żeby pokazać,
+		//że się da, równie dobrze można to przenieść do Start() i nikt by nie płakał
+		public void Initialize() {
 			Destroy(gameObject, 5f);
 		}
 	}
