5f6ccc4c3a6e784685c483d85167c85eefdc154d
Paweł Dziedzic <blavikenn@gmail.com>
2018-05-03 15:39:43 +0200

    Initial 4

---
+++ 4. VFX/Assets/2. Completed/_Scripts/Audio/MixLevels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Completed {
+	public class CompMixLevels : MonoBehaviour {
+
+		public AudioMixer masterMixer;
+
+		public void SetSfxLvl(float sfxLvl) {
+			masterMixer.SetFloat("sfxVol", sfxLvl);
+		}
+
+		public void SetMusicLvl(float musicLvl) {
+			masterMixer.SetFloat("musicVol", musicLvl);
+		}
+	}
+}
---
+++ 4. VFX/Assets/2. Completed/_Scripts/Helpers/VolumeHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Completed {
+	public class VolumeHandler : MonoBehaviour {
+
+		// Use this for initialization
+		void Start() {
+			if (GameObject.Find("EffectsSlider"))
+				GameObject.Find("EffectsSlider").GetComponent<Slider>().onValueChanged.AddListener(SetVolume);
+		}
+
+		void SetVolume(float volume) {
+			GetComponent<AudioSource>().volume = volume;
+		}
+
+		void OnDestroy() {
+			if (GameObject.Find("EffectsSlider"))
+				GameObject.Find("EffectsSlider").GetComponent<Slider>().onValueChanged.RemoveListener(SetVolume);
+		}
+	}
+}
